06a1595616613941800b057b56a505c6e4da87ef
nmilcoff <nicolasmilcoff@gmail.com>
2017-03-02 21:52:39 -0300

    Add objc flag to LinkWith // Add missing file and edit gitignore (#4)

--- Xamarin.OAStackView/libOAStackView.linkwith.cs
+++ Xamarin.OAStackView/libOAStackView.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith ("libOAStackView.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.Simulator64, SmartLink = true, ForceLoad = true)]
+[assembly: LinkWith("libOAStackView.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.Simulator64, SmartLink = true, ForceLoad = true, LinkerFlags = "-ObjC")]
